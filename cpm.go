4b3a22f40c3b45f69b6ab81fb0964cdab2a7208c
Nicklas Körtge <nicklas.koertge@protonmail.com>
2022-05-15 16:17:33 +0200

    error handling, reset env var when build tag not set (#3)
    error handling, reset env var when build tag not set

--- cpm.go
+++ cpm.go
@@ -1,8 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"github.com/n1ckl0sk0rtge/cpm/cmd"
+	"os"
 )
+
+func init() {
+	err := os.Setenv("CPM_DEV_MODE", "false")
+	if err != nil {
+		fmt.Println(err)
+	}
+}
 
 func main() {
 	cmd.Execute()
