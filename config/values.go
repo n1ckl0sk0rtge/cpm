1238f7f83bdfa3fe1cc7dab4376265a10a2299a5
n1ckl0s <25745329+n1ckl0sk0rtge@users.noreply.github.com>
2022-05-15 15:13:59 +0200

    add enum for conf keys

--- config/values.go
+++ config/values.go
@@ -1,9 +1,15 @@
 package config
 
+const (
+	Container string = "container"
+	Runtime   string = "runtime"
+	ExecPath  string = "path"
+)
+
 func GetConfigStructure() *map[string]string {
 	config := make(map[string]string)
-	config["runtime"] = "docker"
-	config["path"] = "/usr/local/bin/"
-	config["container"] = "{}"
+	config[Runtime] = "docker"
+	config[ExecPath] = "/usr/local/bin/"
+	config[Container] = "{}"
 	return &config
 }
