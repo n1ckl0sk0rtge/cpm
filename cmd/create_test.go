f9780a3217d73820f98bc7b32ffdd6de52c4398a
n1ckl0s <25745329+n1ckl0sk0rtge@users.noreply.github.com>
2022-06-01 16:10:21 +0200

    add atomic test config and renaming

--- cmd/create_test.go
+++ cmd/create_test.go
@@ -10,7 +10,12 @@ import (
 	"testing"
 )
 
+var testDir = config.GetTestConfigProperties("init").Dir
+
 func TestCreate(t *testing.T) {
+	test := "testCreate"
+	_ = config.InitTestConfig(test)
+	defer config.RemoveTestConfig(test)
 
 	// Test name contains unwanted char's
 	output := helper.CatchStdOut(t, func() {
@@ -30,35 +35,17 @@ func TestCreate(t *testing.T) {
 	})
 	assert.Equal(t, "provided image is not valid. Please check format\n", output)
 
-	testDir := config.GetTestConfigProperties("init").Dir
-
-	// Tests
-	name := "busybox"
+	// Test with runtime flag
+	name := "testRuntime"
+	entity = flags{Runtime: "testRuntime"}
 	create(nil, []string{name, "busybox:latest"})
 	filename, _ := filepath.Abs(testDir + name)
 	alias, err := ioutil.ReadFile(filename)
 	assert.NoError(t, err)
-	assert.Equal(t, "#!/bin/sh\npodman run -i -t --rm --name "+name+" busybox:latest  \"$@\"\n", string(alias))
-	_ = os.Remove(testDir + name)
-
-	name = "testRuntime"
-	entity = flags{Runtime: "testRuntime"}
-	create(nil, []string{name, "busybox:latest"})
-	filename, _ = filepath.Abs(testDir + name)
-	alias, err = ioutil.ReadFile(filename)
-	assert.NoError(t, err)
 	assert.Equal(t, "#!/bin/sh\ntestRuntime run  --name "+name+" busybox:latest  \"$@\"\n", string(alias))
 	_ = os.Remove(testDir + name)
 
-	name = "testTag"
-	entity = flags{Tag: "1.0.0"}
-	create(nil, []string{name, "busybox"})
-	filename, _ = filepath.Abs(testDir + name)
-	alias, err = ioutil.ReadFile(filename)
-	assert.NoError(t, err)
-	assert.Equal(t, "#!/bin/sh\npodman run  --name "+name+" busybox:1.0.0  \"$@\"\n", string(alias))
-	_ = os.Remove(testDir + name)
-
+	// Test with command flag
 	name = "testCommand"
 	entity = flags{Command: "sh"}
 	create(nil, []string{name, "busybox"})
@@ -68,6 +55,17 @@ func TestCreate(t *testing.T) {
 	assert.Equal(t, "#!/bin/sh\npodman run  --name "+name+" busybox:latest sh \"$@\"\n", string(alias))
 	_ = os.Remove(testDir + name)
 
+	// Test with tag flag
+	name = "testTag"
+	entity = flags{Tag: "1.0.0"}
+	create(nil, []string{name, "busybox"})
+	filename, _ = filepath.Abs(testDir + name)
+	alias, err = ioutil.ReadFile(filename)
+	assert.NoError(t, err)
+	assert.Equal(t, "#!/bin/sh\npodman run  --name "+name+" busybox:1.0.0  \"$@\"\n", string(alias))
+	_ = os.Remove(testDir + name)
+
+	// Test with parameter flag
 	name = "testParameter"
 	entity = flags{Parameter: "-i"}
 	create(nil, []string{name, "busybox"})
@@ -77,4 +75,13 @@ func TestCreate(t *testing.T) {
 	assert.Equal(t, "#!/bin/sh\npodman run -i --name "+name+" busybox:latest  \"$@\"\n", string(alias))
 	_ = os.Remove(testDir + name)
 
+	// Test default
+	name = "busybox"
+	entity = flags{}
+	create(nil, []string{name, "busybox:latest"})
+	filename, _ = filepath.Abs(testDir + name)
+	alias, err = ioutil.ReadFile(filename)
+	assert.NoError(t, err)
+	assert.Equal(t, "#!/bin/sh\npodman run  --name "+name+" busybox:latest  \"$@\"\n", string(alias))
+	_ = os.Remove(testDir + name)
 }
